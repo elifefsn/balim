648f440866a2e8b21e1e3950e1dd84bd64e2fcdd
elifefsn <ucurrumcicegi@gmail.com>
2025-06-13 14:22:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+
+  // Kalp yerine yıldırım ve kalp karışık
+  const symbols = ["❤️", "⚡", "💥"];
+  heart.textContent = symbols[Math.floor(Math.random() * symbols.length)];
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 25 + 15 + "px";
+  document.querySelector(".hearts").appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 4000);
+}
+
+setInterval(createHeart, 250);
